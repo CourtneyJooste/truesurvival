357e354a9227c97b9b1e8303447536c54725cc9f
Courtney Jooste <courtney@codelab.io>
2020-01-16 15:05:19 +0200

    chore: updated address, changed api key and removed stellenbosch

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){});
 
 function initialize() {
-    var myLatlng1 = new google.maps.LatLng(-34.080013, 18.847993);
+    var myLatlng1 = new google.maps.LatLng(-34.088398, 18.842346);
     var mapOptions = {
         zoom : 16,
         center : myLatlng1,
@@ -14,7 +14,7 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById('map1'),
             mapOptions);
 
-    var contentString1 = '<b>TRUE Survival Somerset West</b><br>159 Main Road'
+    var contentString1 = '<b>TRUE Survival Somerset West</b><br>75 Victoria Road (1st Floor)'
 
     var infowindow = new google.maps.InfoWindow({});
 
@@ -30,30 +30,30 @@ function initialize() {
 
     });
 
-    var myLatlng2 = new google.maps.LatLng(-33.934561, 18.854858);
-    var mapOptions2 = {
-        zoom : 16,
-        center : myLatlng2,
-        scrollwheel: false,
-        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"42"},{"lightness":"-43"},{"visibility":"simplified"},{"gamma":"7.59"},{"weight":"0.75"},{"color":"#9b2743"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"},{"color":"#009ca6"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#9b2743"},{"saturation":"-59"},{"lightness":"30"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff1800"},{"saturation":"2"},{"lightness":"2"},{"weight":"0.75"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"-51"},{"color":"#cbcbcb"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2c3e50"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#009ca6"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#041e42"}]},{"featureType":"water","elementType":"labels","stylers":[{"color":"#475777"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"weight":"1.00"},{"color":"#475777"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]}]
-        };
+    // var myLatlng2 = new google.maps.LatLng(-33.934561, 18.854858);
+    // var mapOptions2 = {
+    //     zoom : 16,
+    //     center : myLatlng2,
+    //     scrollwheel: false,
+    //     styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"42"},{"lightness":"-43"},{"visibility":"simplified"},{"gamma":"7.59"},{"weight":"0.75"},{"color":"#9b2743"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"},{"color":"#009ca6"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#9b2743"},{"saturation":"-59"},{"lightness":"30"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff1800"},{"saturation":"2"},{"lightness":"2"},{"weight":"0.75"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"-51"},{"color":"#cbcbcb"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2c3e50"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#009ca6"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#041e42"}]},{"featureType":"water","elementType":"labels","stylers":[{"color":"#475777"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"weight":"1.00"},{"color":"#475777"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]}]
+    //     };
 
-    var map2 = new google.maps.Map(document.getElementById('map2'),
-            mapOptions2);
+    // var map2 = new google.maps.Map(document.getElementById('map2'),
+    //         mapOptions2);
 
-    var contentString2 = '<b>TRUE Survival Stellenbosch</b><br>Van Der Stel'
+    // var contentString2 = '<b>TRUE Survival Stellenbosch</b><br>Van Der Stel'
 
-    var infowindow = new google.maps.InfoWindow({});
+    // var infowindow = new google.maps.InfoWindow({});
 
-    var marker2 = new google.maps.Marker({
-        position : myLatlng2,
-        map : map2,
-        title : 'TRUE Survival Stellenbosch'
-    });
-    google.maps.event.addListener(marker2, 'click', function initialize() {
-        infowindow.close();//hide the infowindow
-        infowindow.setContent(contentString2);//update the content for this marker
-        infowindow.open(map2, marker2);
+    // var marker2 = new google.maps.Marker({
+    //     position : myLatlng2,
+    //     map : map2,
+    //     title : 'TRUE Survival Stellenbosch'
+    // });
+    // google.maps.event.addListener(marker2, 'click', function initialize() {
+    //     infowindow.close();//hide the infowindow
+    //     infowindow.setContent(contentString2);//update the content for this marker
+    //     infowindow.open(map2, marker2);
 
-    });
+    // });
 }
